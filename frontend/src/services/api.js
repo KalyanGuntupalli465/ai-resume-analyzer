63ae6397111d3c758a2a337f83bca6009b91c890
gmk <gmk@example.com>
2026-05-12 15:13:00 -0500

    deployed backend link updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://ai-resume-analyzer-backend-5sk7.onrender.com",
 });
 
 export default API;
